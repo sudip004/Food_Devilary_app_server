873d5459ef1f7123efdb0595566594cc70bf8fa3
Sudip Basak <84569249+sudip004@users.noreply.github.com>
2025-10-29 12:28:11 +0530

    Update BaseClass.java

--- src/main/java/com/food/server/controller/BaseClass.java
+++ src/main/java/com/food/server/controller/BaseClass.java
@@ -2,7 +2,7 @@ package com.food.server.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:5173" , "https://restorafood.netlify.app"}, allowCredentials = "true")
 public abstract class BaseClass {
     
 }
